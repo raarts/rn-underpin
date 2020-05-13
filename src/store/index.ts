eb9e43500f7b9467a6be339e3bbe0c04d65570d6
Ron Arts <ron.arts@gmail.com>
2020-05-13 12:05:35 +0200

    Logging redux actions. yarn add redux-logger; yarn add @types/redux-logger -D

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
+import { createLogger } from 'redux-logger';
 import { AsyncStorage } from 'react-native';
 import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
 import darkModeReducer from './darkmode';
@@ -14,13 +15,30 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
+// build the list of middleware to apply to store actions
+const middleware = [
+  ...getDefaultMiddleware({
     serializableCheck: {
       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
     },
   }),
+];
+
+// redux-logger. Only activate when needed. This should be the last entry in the middleware array
+const { pathname } = window.location || {};
+const IS_RUNNING_IN_CHROME = pathname && pathname.indexOf('debugger-ui');
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const logger = createLogger({
+  // only log if the predicate function returns true
+  predicate: (getState, action) => action.type !== '',
+});
+if (IS_RUNNING_IN_CHROME && !IS_PRODUCTION) {
+  middleware.push(logger);
+}
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware,
 });
 
 export const persistor = persistStore(store);
