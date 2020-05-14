f68b45d2aebdd6067b5c2a7a79566108ce01f4c8
Ron Arts <ron.arts@gmail.com>
2020-05-14 14:54:17 +0200

    yarn add @react-navigation/native expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view To start, and... yarn add @react-navigation/stack for the stacknavigator. Also removed al ts warnings. Proud!

---
+++ src/screens/TemplateScreen.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { Image, Switch, Text, View } from 'react-native';
+import { ReactElement } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { _ } from '../i18n';
+import ThemeProvider, { useTheme } from '../underpin/ThemeProvider';
+import { RootState } from '../store';
+import { useViewport } from '../underpin/ViewportProvider';
+import { setDarkMode } from '../store/theme';
+
+const TemplateScreen = (): ReactElement => {
+  const dispatch = useDispatch();
+  const styles = useTheme(baseStyles);
+  const { darkMode } = useSelector((state: RootState) => state.theme);
+  const { viewportWidth, viewportHeight, viewportOrientation, viewportFormFactor } = useViewport();
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/images/robot-dev.png')} style={styles.welcomeImage} />
+      <Text style={styles.color}>{_('openAppAndStart')}</Text>
+      <Text style={styles.color}>{`screen: ${viewportWidth} x ${viewportHeight}`}</Text>
+      <Text style={styles.color}>{`This looks like a ${viewportFormFactor} in ${viewportOrientation} mode`}</Text>
+      <Switch
+        value={darkMode === 'dark'}
+        onValueChange={(v): void => {
+          dispatch(setDarkMode(v));
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = ThemeProvider.create({
+  container: {
+    flex: 1,
+    backgroundColor: '$backgroundColor',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  color: {
+    color: '$textColor',
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+});
+const baseStyles = styles;
+
+export default TemplateScreen;
