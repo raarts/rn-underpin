5abfc11e2756d48c7743c7c094491d59120b57cb
Ron Arts <ron.arts@gmail.com>
2020-05-21 11:01:04 +0200

    Implement web fast refresh as described here: https://dev.to/raarts/comment/p3h1

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
-module.exports = function(api) {
-  api.cache(true);
+/* eslint-disable @typescript-eslint/explicit-function-return-type, func-names */
+module.exports = function (api) {
+  // This caches the Babel config by environment.
+  api.cache.using(() => process.env.NODE_ENV);
+
   return {
     presets: ['babel-preset-expo'],
   };
